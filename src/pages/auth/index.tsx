657ea23da4eb24e5431ab031279f8083e57149e7
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-06-30 22:06:09 -0400

    Create the navbar component

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar';
+
+const AuthPage = () => {
+  return (
+    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+      <div className='max-w-7xl mx-auto'>
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
