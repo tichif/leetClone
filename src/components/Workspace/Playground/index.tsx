6475d64949a624229847c4c4557d74cc294cbefe
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-08-01 18:42:25 -0400

    Create Code Editor Footer

--- src/components/Workspace/Playground/index.tsx
+++ src/components/Workspace/Playground/index.tsx
@@ -4,6 +4,7 @@ import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
 
 import PreferencesNav from './PreferencesNav';
+import EditorFooter from './EditorFooter';
 
 type Props = {};
 
@@ -24,8 +25,57 @@ const Playground = (props: Props) => {
             style={{ fontSize: 16 }}
           />
         </div>
-        <div>test cases</div>
+        <div className='w-full px-5 overflow-auto'>
+          {/* heading */}
+          <div className='flex h-10 items-center space-x-6'>
+            <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+              <div className='text-sm font-medium leading-5 text-white'>
+                Test Cases
+              </div>
+              <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+            </div>
+          </div>
+          {/* testcases */}
+          <div className='flex'>
+            {/* case 1 */}
+            <div className='mr-2 items-start mt-2 text-white'>
+              <div className='flex flex-wrap gap-y-4 items-center'>
+                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                  Case 1
+                </div>
+              </div>
+            </div>
+            {/* case 2 */}
+            <div className='mr-2 items-start mt-2 text-white'>
+              <div className='flex flex-wrap gap-y-4 items-center'>
+                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                  Case 2
+                </div>
+              </div>
+            </div>
+            {/* case 3 */}
+            <div className='mr-2 items-start mt-2 text-white'>
+              <div className='flex flex-wrap gap-y-4 items-center'>
+                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                  Case 3
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* test */}
+          <div className='font-semibold my-4'>
+            <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+              nums:[2,7,11,15], target: 9
+            </div>
+            <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+              [0,1]
+            </div>
+          </div>
+        </div>
       </Split>
+      <EditorFooter />
     </div>
   );
 };
